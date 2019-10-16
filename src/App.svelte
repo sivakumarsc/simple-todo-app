363d786bd4a2c8b89f8e338fabeeba023fafae62
scsiva1991 <scsiva1991@gmail.com>
2019-10-16 20:48:40 +0530

    svelte todo app setup

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let todo = '';
+	export let todos = [];
+
+	function addTodo(e) {
+		if (e.keyCode === 13 && todo.trim()) {
+			todos = [...todos, {
+				id: todos.length,
+				task: todo,
+				isCompleted: false,
+			}];
+			todo = '';
+		}
+	}
+
+	function updateTodo() {
+		
+	}
+</script>
+
+<input on:keydown={addTodo} bind:value={todo} placeholder="Type here...">
+{todos.length}
+<ul>
+	{#each todos as todo}
+		<li>{todo.task}</li>
+	{/each}
+</ul>
